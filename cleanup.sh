c706ce6e9dd1a7bc00a0e32b19e1b2dc5a197168
Brian Redmond <brian.redmond@live.com>
2016-10-18 17:06:35 -0400

    Updates from Mani

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
-azure servicefabric service remove fabric:/SimpleContainerApp/StatelessFrontendService
-azure servicefabric service remove fabric:/SimpleContainerApp/StatelessBackendService
-azure servicefabric application remove fabric:/SimpleContainerApp
+azure servicefabric service delete fabric:/SimpleContainerApp/StatelessFrontendService
+azure servicefabric service delete fabric:/SimpleContainerApp/StatelessBackendService
+azure servicefabric application delete fabric:/SimpleContainerApp
 azure servicefabric application type unregister SimpleContainerApp 1.0
